b5f51c5e8d6228d01e59008fc1545952035c4d78
Вероника Магдысюк <Veronika.khust.1998@gmail.com>
2020-03-15 15:08:12 +0200

    фикс

---
+++ ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimsovisionDataBase.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        public string Id { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Новый пароль")]
+        [Required(ErrorMessage = "Поле не может быть пустым.")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+    }
+}
